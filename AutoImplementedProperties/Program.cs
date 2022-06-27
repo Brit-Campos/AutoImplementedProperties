d360dd4c6f8b9a686ef8c5161f947ab9d8108c93
Brittanie Campos <brittanie.angelique@gmail.com>
2022-06-27 15:04:23 -0700

    Add project files.

---
+++ AutoImplementedProperties/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Auto-Implemented properties = shortcut when no additional logic is required in the property
+        //                               you do not have to define a field for a property
+        //                               you only have to write get; and/or set; inside the property
+
+        Car car = new Car("Porsche");
+
+        Console.WriteLine(car.Model);
+
+        Console.ReadKey();
+
+    }
+}
+
+class Car
+{
+    String model;
+
+    public String Model { get; set; }
+
+    public Car(String model)
+    {
+        this.Model = model;
+    }
+}
